e720685e4a50ce4c260edd652c08bbf71e7e3035
Velvet <rafifadillah420@gmail.com>
2025-04-04 16:06:50 +0800

    Implementasi program array menggunakan C++

---
+++ Array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+string kelas[30];
+int pos = 0; //posisi data dimasukan
+void inputData(string nama); //deklarasi fungsi precuder input data
+void outputData(); //deklarasi fungsi precuder output data
+
+int main() {
+
+  //cout << "Hello World!";
+  inputData("Muhammad Rafi Fadilah");
+
+  inputData("Masjid");
+  outputData();
+  inputData("Langgar");
+  outputData();
+  inputData("Ustad");
+  outputData();
+
+
+  return 0;
+}
+
+
+void inputData(string nama){
+    kelas[pos] = nama; //masukkan data ke array
+    pos++;
+}
+
+void inputData(string nama){
+    kelas[pos] = nama; //masukkan data ke array
+    pos++;
+    //cout << "Hello Word!";
+}
+
+void outputData(){
+    for (int i = 0; i < pos; i++)
+    {
+        cout << kelas[i];
+        if (i<pos-1)
+    {
+        cout << ", ";
+    }else{
+        cout <<"."<<endl;
+    }
+
+}
